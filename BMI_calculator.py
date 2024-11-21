f1091deddceae0b5542a2dc56a0f0e74345fb3df
Hiwot Wondale <105517470+Hiwiye@users.noreply.github.com>
2024-11-21 13:40:17 +0300

    added interpretation for BMI

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -2,3 +2,9 @@ weight = float(input("what is your weight in kg?"))
 height = float(input("what is your height in m?"))
 BMI = weight/height ** 2
 print("Your BMI is", BMI ) 
+if BMI < 18.5:
+    print("underweight")
+elif BMI < 25:
+    print("normal weight")
+else:
+    print("over weight")
