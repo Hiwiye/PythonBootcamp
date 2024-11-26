abd03f81205c4d4b349781acabfc53e9f692cc75
Dr. Hiwot Wondale <hiwotwondalemd@gmail.com>
2024-11-26 10:45:00 +0300

    Tip Calculator

--- Tip Calculator/Tip_calculator.py
+++ Tip Calculator/Tip_calculator.py
@@ -3,5 +3,5 @@ Bill = float(input("What was the total bill?"))
 Tip = int(input("What percentage tip would you like to give?"))
 people = int(input("How many people will split the bill?"))
 money = float((Bill * Tip/100)+ 150)/5
-formatted_money = "{:.2f}".format(money)
-print("Each person should pay", formatted_money)
+final_amount = round(money, 2)
+print(f"Each person should pay: {final_amount}")
